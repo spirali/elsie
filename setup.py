db59c69554bccac3a256f31abe1728c81df5a384
Stanislav Bohm <stanislav.bohm@vsb.cz>
2018-06-08 16:22:39 +0200

    ENH setup.py improved

--- setup.py
+++ setup.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 
 import sys
 
 if sys.version_info.major < 3 or \
-       (sys.version_info.major == 3 and sys.version_info.minor < 2):
-    sys.exit("Python 3.2 or newer is required")
+       (sys.version_info.major == 3 and sys.version_info.minor < 4):
+    sys.exit("Python 3.4 or newer is required")
 
 setup(name='elsie',
-      version='1.0',
+      version='1.1',
       description='Framework for making slides',
+      long_description="""
+Elsie is a Framework for making slides in Python,
+see http://github.com/spirali/elsie for an example.
+      """,
       author='Stanislav Bohm',
       url='http://github.com/spirali/elsie',
-      packages=['elsie'])
+      packages=['elsie'],
+      install_requires=["pypdf2", "pygments", "lxml"],
+      classifiers=("Programming Language :: Python :: 3",
+                   "License :: OSI Approved :: MIT License",
+                   "Operating System :: OS Independent"))
