f67deb971d4ff203d273569f1030af5d9cb09dec
Stanislav Bohm <stanislav.bohm@vsb.cz>
2019-01-05 13:55:46 +0100

    Version changed to 1.3

--- setup.py
+++ setup.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python
 
 from setuptools import setup
-
 import sys
 
 if sys.version_info.major < 3 or \
        (sys.version_info.major == 3 and sys.version_info.minor < 4):
     sys.exit("Python 3.4 or newer is required")
 
+
+VERSION = None
+with open("elsie/version.py") as f:
+     exec(f.read())
+if VERSION is None:
+    raise Exception("version.py executed but VERSION was not set")
+
 setup(name='elsie',
-      version='1.2',
+      version=VERSION,
       description='Framework for making slides',
       long_description="""
 Elsie is a Framework for making slides in Python,
