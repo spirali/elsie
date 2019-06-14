bc92cabd596cb2c264afb930234d3ef8dac53faf
Stanislav Bohm <spirali@kreatrix.org>
2019-06-14 16:57:13 +0200

    TEST: cache test improved

--- tests/test_others.py
+++ tests/test_others.py
@@ -17,6 +17,7 @@ def test_next(test_env):
 
 
 def test_cache(test_env):
+    cache_dir = "elsie-cache"
     slides = Slides()
 
     slide = slides.new_slide()
@@ -24,11 +25,44 @@ def test_cache(test_env):
     slide.box(show="next+").text("2")
     slide.box(show="next+").text("3")
 
-    cache_dir = "elsie-cache"
+    assert not glob.glob("{}/*".format(cache_dir))
     slides.render(output="test.pdf", cache_dir=cache_dir)
 
-    assert set(os.path.basename(p) for p in glob.glob("{}/*.pdf".format(cache_dir))) == {
+    assert set(os.path.basename(p) for p in glob.glob("{}/*".format(cache_dir))) == {
         "7cdc642e30c1a38b89bf9bb00bcc0f60fc9617bf.pdf",
         "748bb89d5eb54a97933e39f406b1e6ed25d0b219.pdf",
-        "f3362fd290216e782a7ced435ca2ceff011776d1.pdf"
+        "f3362fd290216e782a7ced435ca2ceff011776d1.pdf",
+        "queries3.cache"
+    }
+
+    slides = Slides()
+
+    slide = slides.new_slide()
+    slide.box(show="next+").text("1")
+    slide.box(show="next+").text("2")
+    slide.box(show="next+").text("3")
+
+    slides.render(output="test.pdf", cache_dir=cache_dir)
+
+    assert set(os.path.basename(p) for p in glob.glob("{}/*".format(cache_dir))) == {
+        "7cdc642e30c1a38b89bf9bb00bcc0f60fc9617bf.pdf",
+        "748bb89d5eb54a97933e39f406b1e6ed25d0b219.pdf",
+        "f3362fd290216e782a7ced435ca2ceff011776d1.pdf",
+        "queries3.cache"
+    }
+
+    slides = Slides()
+
+    slide = slides.new_slide()
+    slide.box(show="next+").text("1")
+    slide.box(show="next+").text("2")
+    slide.box(show="next+").text("4")
+
+    slides.render(output="test.pdf", cache_dir=cache_dir)
+
+    assert set(os.path.basename(p) for p in glob.glob("{}/*".format(cache_dir))) == {
+        "7cdc642e30c1a38b89bf9bb00bcc0f60fc9617bf.pdf",
+        "4e9e0eb2550172bf1c96e0485333d31cb9026c3c.pdf",
+        "f3362fd290216e782a7ced435ca2ceff011776d1.pdf",
+        "queries3.cache"
     }
