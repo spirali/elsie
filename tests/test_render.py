04780631b062b41b6fba28634dea35b586b89c8e
Jakub Beránek <berykubik@gmail.com>
2020-11-02 21:02:09 +0100

    Add CI and documentation bootstrap

--- tests/test_render.py
+++ tests/test_render.py
@@ -9,6 +9,7 @@ def test_drop_same_consecutive_slides(test_env):
     test_env.check("drop", render_args=dict(drop_duplicates=True))
 """
 
+
 def test_keep_same_nonconsecutive_slides(test_env):
     slide = test_env.slide
     slide.box().text("Drop")
