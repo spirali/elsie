a348342ad7c7650c85d840435be4f9c121ada49a
Jakub Beránek <berykubik@gmail.com>
2020-12-28 19:52:22 +0100

    Add ordered lists

--- elsie/ext/list.py
+++ elsie/ext/list.py
@@ -1,136 +1,132 @@
-from contextlib import contextmanager
-from typing import Optional
+from typing import List
 
 from ..boxtree.box import Box
 
 
 class ListBuilder:
-    """
-    This class serves as a helper for creating simple unordered bullet lists.
-    It can draw list items in a column with a specified bullet point and it also handles
-    indentation of sublists.
-
-    Individual list items are created with the `item` method.
-    Parameters passed to the `item` method override the default list item attributes from the
-    constructor.
-
-    Example:
-        lst = ListBuilder(slide.box())
-        lst.item().text("Item 1")
-        lst.item(level=1).text("Nested item")
-        lst.item().text("Item 2")
-    """
-
     def __init__(
         self,
         parent: Box,
+        label_render_fn,
+        default_indent,
         *,
-        bullet: Optional[str] = "•",
-        bullet_width=25,
-        level_indent=25,
-        show="last+",
+        label_padding: int,
+        level=None,
+        start=1,
+        total_indent=0,
         **box_args
     ):
-        """
-        Parameters
-        ----------
-        parent: Box
-            Box parent of the list.
-        bullet: str
-            Default character for the bullet point. If you do not want to draw bullet points,
-            pass `None`.
-        bullet_width: float
-            Width of the bullet box in pixels.
-        level_indent: float
-            Horizontal gap before each indentation level.
-        show: str
-            Default fragment selector for a list item.
-        box_args: kwargs
-            Additional parameters passed to each list item box.
-        """
         self.parent = parent
-        self.bullet = bullet
-        self.bullet_width = bullet_width
-        self.level_indent = level_indent
-        self.show = show
-        self.level = 0
-        self.box_args = box_args
+        self.default_indent = default_indent
+        self.label_padding = label_padding
+        self.box_args = dict(box_args)
+        if "show" not in self.box_args:
+            self.box_args["show"] = "last+"
 
-    def item(
-        self, level: int = None, show: str = None, bullet: str = None, **box_args
-    ) -> Box:
-        """
-        Create a new list item.
+        self.level = [] if level is None else list(level)
+        self.level.append(start - 1)
+        self.total_indent = total_indent
+        self.label_render_fn = label_render_fn
 
-        If the indentation level is not specified, the current indentation level of the ListBuilder
-        will be used.
+    def __enter__(self):
+        return self
 
-        Parameters
-        ----------
-        level: int
-            Indentation level of the item.
-        show: str
-            Fragment selector of the item.
-        bullet: str
-            Bullet character of the item.
-        box_args: kwargs
-            Additional parameters pass to the box of the item.
-        """
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        pass
+
+    def ul(self, indent=None, label_render_fn=None, **box_args):
+        indent = indent if indent is not None else self.default_indent
+        box, total_indent = self._create_box(self.parent, indent=indent, **box_args)
+        return ListBuilder(
+            box,
+            label_render_fn or default_ul_render_fn,
+            indent,
+            level=self.level,
+            total_indent=total_indent,
+            label_padding=self.label_padding,
+            **self.box_args
+        )
+
+    def ol(self, level=None, *, start=1, indent=None, label_render_fn=None, **box_args):
+        assert start >= 1
+
+        indent = indent if indent is not None else self.default_indent
+        box, total_indent = self._create_box(self.parent, indent=indent, **box_args)
+        level = level if level is not None else self.level
+        return ListBuilder(
+            box,
+            label_render_fn or default_ol_render_fn,
+            indent,
+            level=level,
+            total_indent=total_indent,
+            start=start,
+            label_padding=self.label_padding,
+            **self.box_args
+        )
+
+    def item(self, label: str = None, label_padding: int = None, **box_args):
+        self.level[-1] += 1
+
+        label = label if label is not None else self.label_render_fn(self.level)
+        label_padding = (
+            label_padding if label_padding is not None else self.label_padding
+        )
+
         args = dict(self.box_args)
         args.update(box_args)
-        level = self.level if level is None else level
-        show = self.show if show is None else show
-        bullet = self.bullet if bullet is None else bullet
+        box_args["horizontal"] = True
+        box, _ = self._create_box(self.parent, indent=0, **box_args)
 
-        b = self.parent.box(
-            x=level * self.level_indent, horizontal=True, show=show, **args
-        )
-        if bullet is not None:
-            b.box(width=self.bullet_width, y=0).text(bullet)
-        return b.box(width="fill")
+        if label is not None:
+            box.box().text(label)
+        return box.box(p_left=label_padding, width="fill")
 
-    @contextmanager
-    def indent_scope(self, levels=1):
-        """
-        Context manager that changes the indentation level in its scope.
+    def _create_box(self, parent: Box, indent=None, **box_args):
+        new_indent = self.default_indent if indent is None else indent
+        total_indent = self.total_indent + new_indent
 
-        Example:
-            lst = ListBuilder(slide.box())
-            with lst.indent_scope():
-                # items created here will have level = 1
-                with lst.indent_scope():
-                    # items created here will have level = 1
-        Parameters
-        ----------
-        levels: int
-            How many levels of indentation to apply in the scope of the context manager.
-        """
-        self.indent(levels)
-        try:
-            yield
-        finally:
-            self.dedent(levels)
+        args = dict(self.box_args)
+        args.update(box_args)
+        return parent.box(x="[0%]", p_left=total_indent, **args), total_indent
 
-    def indent(self, levels=1):
-        """
-        Increate the indentation level of the builder by the given amount of levels.
 
-        Parameters
-        ----------
-        levels: int
-            Amount of levels to increase the current indetation level.
-        """
-        self.level += levels
+def unordered_list(
+    parent: Box, indent=10, label_padding=25, label_render_fn=None, **default_box_args
+) -> ListBuilder:
+    return ListBuilder(
+        parent,
+        default_indent=indent,
+        label_padding=label_padding,
+        label_render_fn=label_render_fn or default_ul_render_fn,
+        **default_box_args
+    )
 
-    def dedent(self, levels=1):
-        """
-        Decrease the indentation level of the builder by the given amount of levels.
 
-        Parameters
-        ----------
-        levels: int
-            Amount of levels to decrease the current indetation level.
-        """
-        if self.level - levels < 0:
-            raise Exception("Cannot change indent level below zero")
-        self.level -= levels
+def ordered_list(
+    parent: Box,
+    indent=10,
+    start=1,
+    label_padding=25,
+    label_render_fn=None,
+    **default_box_args
+) -> ListBuilder:
+    return ListBuilder(
+        parent,
+        default_indent=indent,
+        label_padding=label_padding,
+        start=start,
+        label_render_fn=label_render_fn or default_ol_render_fn,
+        **default_box_args
+    )
+
+
+def arabic_digit_render_fn(level: List[int]) -> str:
+    return ".".join(str(item) for item in level) + "."
+
+
+def bullet_point_render_fn(level: List[int]) -> str:
+    return "•"
+
+
+default_ul_render_fn = bullet_point_render_fn
+default_ol_render_fn = arabic_digit_render_fn
--- tests/ext/test_lists.py
+++ tests/ext/test_lists.py
@@ -1,9 +1,9 @@
-from elsie.ext import ListBuilder
+from elsie.ext import ordered_list, unordered_list
 
 
 def test_list_basic(test_env):
     slide = test_env.slide
-    lst = ListBuilder(slide.box())
+    lst = unordered_list(slide.box())
     lst.item().text("Item 1")
     lst.item().text("Item 2")
     lst.item().text("Item 3")
@@ -11,71 +11,99 @@ def test_list_basic(test_env):
     test_env.check("list-basic")
 
 
+def test_list_with(test_env):
+    slide = test_env.slide
+    lst = unordered_list(slide.box())
+    with lst.ul() as l:
+        with l.ul():
+            pass
+
+
 def test_list_override(test_env):
     slide = test_env.slide
-    lst = ListBuilder(slide.box())
-    lst.item(bullet="x").text("Item 1")
+    lst = unordered_list(slide.box(), label_padding=20, indent=50)
+    lst.item(label="x", label_padding=10).text("Item 1")
     lst.item(show="next+", height=200).text("Item 2")
-    lst.item().text("Item 3")
+    lst.ul().item().text("Item 3")
+    lst.ul(indent=5).item().text("Text 4")
 
     test_env.check("list-override", expect_count=2)
 
 
 def test_list_no_bullet(test_env):
     slide = test_env.slide
-    lst = ListBuilder(slide.box(), bullet=None)
+    lst = unordered_list(slide.box(), label_render_fn=lambda _: None)
     lst.item().text("Item 1")
     lst.item().text("Item 2")
     lst.item().text("Item 3")
 
-    test_env.check("list-no-bullet", bless=True)
+    test_env.check("list-no-bullet")
 
 
-def test_list_indent_explicit(test_env):
+def test_list_indent(test_env):
     slide = test_env.slide
-    lst = ListBuilder(slide.box())
+    lst = unordered_list(slide.box())
     lst.item().text("Item 1")
-    lst.item(level=1).text("Item 2")
-    lst.item(level=1).text("Item 3")
-    lst.item(level=2).text("Item 4")
-    lst.item(level=3).text("Item 5")
-    lst.item(level=1).text("Item 6")
+    l2 = lst.ul()
+    l2.item().text("Item 2")
+    l2.item().text("Item 3")
+    l3 = l2.ul()
+    l3.item().text("Item 4")
+    l3.ul().item().text("Item 5")
+    l2.item().text("Item 6")
     lst.item().text("Item 7")
 
     test_env.check("list-indent")
 
 
-def test_list_indent_stateful(test_env):
+def test_ordered_list(test_env):
     slide = test_env.slide
-    lst = ListBuilder(slide.box())
+    lst = ordered_list(slide.box())
     lst.item().text("Item 1")
-    lst.indent()
-    lst.item().text("Item 2")
-    lst.item().text("Item 3")
-    lst.indent()
+    l2 = lst.ol()
+    l2.item().text("Item 2")
+    l2.item().text("Item 3")
+    l2.ol().item().text("Item X")
+    l2.item().text("Item Y")
     lst.item().text("Item 4")
-    lst.indent()
     lst.item().text("Item 5")
-    lst.dedent(2)
     lst.item().text("Item 6")
-    lst.dedent()
     lst.item().text("Item 7")
 
-    test_env.check("list-indent")
+    test_env.check("list-ordered-indent")
 
 
-def test_list_indent_scope(test_env):
+def test_ordered_list_explicit_level(test_env):
     slide = test_env.slide
-    lst = ListBuilder(slide.box())
+    lst = ordered_list(slide.box())
     lst.item().text("Item 1")
-    with lst.indent_scope():
-        lst.item().text("Item 2")
-        lst.item().text("Item 3")
-        with lst.indent_scope():
-            lst.item().text("Item 4")
-            with lst.indent_scope():
-                lst.item().text("Item 5")
-        lst.item().text("Item 6")
+    l2 = lst.ol(level=(1, 2))
+    l2.item().text("Item 2")
+    l2.item().text("Item 3")
+    l2.ol().item().text("Item X")
+    l2.item().text("Item Y")
+    lst.item().text("Item 4")
+    l3 = lst.ol(level=())
+    l3.item().text("Item 5")
+    l3.item().text("Item 6")
     lst.item().text("Item 7")
 
-    test_env.check("list-indent")
+    test_env.check("list-ordered-explicit-level")
+
+
+def test_combine_ordered_unordered(test_env):
+    slide = test_env.slide
+    lst = ordered_list(slide.box())
+    lst.item().text("Item 1")
+    l2 = lst.ol()
+    l2.item().text("Item 2")
+    l2.item().text("Item 3")
+    l2.ul().item().text("Item X")
+    l2.item().text("Item Y")
+    lst.item().text("Item 4")
+    l3 = lst.ul()
+    l3.item().text("Item 5")
+    l3.ol().item().text("Item 6")
+    lst.item().text("Item 7")
+
+    test_env.check("list-combine-ordered-unordered")
