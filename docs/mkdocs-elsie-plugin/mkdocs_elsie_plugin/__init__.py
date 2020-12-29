28af15cb0cc2e82a472f89a03fa5cfe52d2b991b
Jakub Beránek <berykubik@gmail.com>
2020-12-29 11:52:41 +0100

    Allow choosing between SVG and PNG output in mkdocs-elsie-plugin

--- docs/mkdocs-elsie-plugin/mkdocs_elsie_plugin/__init__.py
+++ docs/mkdocs-elsie-plugin/mkdocs_elsie_plugin/__init__.py
@@ -24,6 +24,7 @@ import contextlib
 import os
 from typing import List
 
+from mkdocs.config.config_options import Choice
 from mkdocs.plugins import BasePlugin
 
 
@@ -83,7 +84,8 @@ def render_slide(code: List[str],
                  height: int,
                  border: bool,
                  debug_boxes: bool,
-                 skip: slice) -> str:
+                 skip: slice,
+                 render_format: str) -> str:
     border_str = """slide.rect(color="black")""" if border else ""
 
     code = code[skip]
@@ -104,7 +106,7 @@ slide = slides.new_slide({', '.join(f"{k}={v}" for (k, v) in slide_args.items())
 {border_str}
 {code}
 
-result = elsie_render_slide(slides._slides[-1], format="png")
+result = elsie_render_slide(slides._slides[-1], format="{render_format}")
 """.strip()
 
     locals = {}
@@ -147,9 +149,14 @@ def iterate_fences(src: str, handle_fence):
 
 
 class ElsiePlugin(BasePlugin):
+    config_scheme = (
+        ("render_format", Choice(("svg", "png"))),
+    )
+
     def on_page_markdown(self, src: str, page, config, *args, **kwargs):
         # TODO: use Markdown parser
 
+        render_format = self.config.get("render_format", "svg")
         docs_dir = config["docs_dir"]
         ctx = CodeContext()
 
@@ -180,7 +187,8 @@ class ElsiePlugin(BasePlugin):
                                          height=height,
                                          border=border == "yes",
                                          skip=skip_slice,
-                                         debug_boxes=debug_boxes == "yes").splitlines(
+                                         debug_boxes=debug_boxes == "yes",
+                                         render_format=render_format).splitlines(
                         keepends=False)
                 return elsie_to_python_header(header), lines
             return header, []
