e3722de6bce5ad137ca0f09f4fa633e75ded620e
Jakub Beránek <berykubik@gmail.com>
2021-01-20 10:12:38 +0100

    Use absolute path for cache directory

--- tests/test_jupyter.py
+++ tests/test_jupyter.py
@@ -1,3 +1,16 @@
+import pytest
+
+from elsie.render.jupyter import render_slide_html
+
+
+@pytest.mark.parametrize("format", ("svg", "png"))
+def test_jupyter_render(test_env, format):
+    slide = test_env.slide
+    slide.box().text("Hello")
+
+    assert render_slide_html(slide.slide, format)
+
+
 def test_no_js_with_single_step(test_env):
     slide = test_env.slide
     slide.box().text("Hello")
