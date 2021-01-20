995d3cfffce89c539a3d7cbb44402d14ab26011d
Jakub Beránek <berykubik@gmail.com>
2021-01-20 16:45:55 +0100

    Remove deprecated test

--- tests/test_render.py
+++ tests/test_render.py
@@ -1,23 +1,5 @@
 from elsie import TextStyle
 
-"""
-def test_drop_same_consecutive_slides(test_env):
-    slide = test_env.slide
-    slide.box().text("Drop")
-    test_env.slides.new_slide().box().text("Drop")
-    test_env.check("drop", render_args=dict(drop_duplicates=True))
-
-
-def test_keep_same_nonconsecutive_slides(test_env):
-    slide = test_env.slide
-    slide.box().text("Drop")
-    test_env.slides.new_slide().box().text("Interlude")
-    test_env.slides.new_slide().box().text("Drop")
-    test_env.check(
-        "drop-interlude", expect_count=3, render_args=dict(drop_duplicates=True)
-    )
-"""
-
 
 def test_postprocessing(test_env):
     called = [False]
