e66daa70f36852c8a8225663cbe1c6d3d4f56a37
Stanislav Bohm <spirali@kreatrix.org>
2021-03-02 16:05:35 +0100

    todo placeholder

---
+++ elsie/ext/todo.py
@@ -0,0 +1,15 @@
+
+
+def todo_placeholder(box, name=None):
+    if name is None:
+        name = box.name
+    todo_box = box.fbox(padding=3)
+    todo_box.rect(bg_color="#ee9", color="black", stroke_width=6)
+    if name:
+        text = f"TODO\n{name}"
+    else:
+        text = "TODO"
+    inner_box = todo_box.fbox(padding=20)
+    inner_box.rect(bg_color="#ee9", color="black", stroke_width=6)
+    inner_box.text(text)
+    return todo_box
